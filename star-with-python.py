1180ee0ee7c55b994f1156bc9048e57165ba962f
Pingu Lee <137541277+PinguLee@users.noreply.github.com>
2023-09-29 20:42:21 +0900

    Update star-with-python.py
    ex1, ex3

--- star-with-python.py
+++ star-with-python.py
@@ -11,4 +11,40 @@ def ex1():
     for i in range(starCount):
         print("*" * (i + 1))
         
+'''
+*****
+****
+***
+**
+*
+'''
+def ex2():
+
+        
+'''
+    *
+   **
+  ***
+ ****
+*****
+'''
+def ex3():
+    for i in range(starCount):
+        print(" " * (starCount - (i + 1)), end = "")
+        print('*' * (i + 1))
+      
+'''
+*****
+ ****
+  ***
+   **
+    *
+'''  
+def ex4():
+
+
+        
 ex1()
+ex2()
+ex3()
+ex4()
